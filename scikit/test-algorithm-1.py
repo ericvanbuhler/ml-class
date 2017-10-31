93e1faf81178cee8124f6c89b60bf85b18017270
Eric VanBuhler <evanbuhl@qti.qualcomm.com>
2017-10-30 23:34:49 -0700

    Reorg and clean up scripts

--- scikit/test-algorithm-1.py
+++ scikit/test-algorithm-1.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 
-
 # Get a pandas DataFrame object of all the data in the csv file:
 df = pd.read_csv('tweets.csv')
 
@@ -11,23 +10,28 @@ text = df['tweet_text']
 # Get pandas Series object of the "emotion" column:
 target = df['is_there_an_emotion_directed_at_a_brand_or_product']
 
+# The rows of  the "emotion" column have one of four strings:
+# 'Positive emotion'
+# 'Negative emotion'
+# 'No emotion toward brand or product'
+# 'I can't tell'
+
 # Remove the blank rows from the series:
-target = target[pd.notnull(text)]
-text = text[pd.notnull(text)]
+fixed_text = text[pd.notnull(text)]
+fixed_target = target[pd.notnull(text)]
 
-# Perform feature extraction:
+# Perform feature extraction and train a model with this data
 from sklearn.feature_extraction.text import CountVectorizer
-count_vect = CountVectorizer()
-count_vect.fit(text)
-counts = count_vect.transform(text)
-
-# Train with this data with a Naive Bayes classifier:
 from sklearn.naive_bayes import MultinomialNB
-nb = MultinomialNB()
-nb.fit(counts, target)
+from sklearn.pipeline import Pipeline
 
-# See what the classifier predicts for some new tweets:
-predictions = nb.predict(counts)
+p = Pipeline(steps=[('counts', CountVectorizer()),
+                ('multinomialnb', MultinomialNB())])
+
+p.fit(fixed_text, fixed_target)
+
+# See what the classifier predicts for some tweets:
+predictions = p.predict(fixed_text)
 print(len(predictions))
-correct_predictions = sum(predictions == target)
+correct_predictions = sum(predictions == fixed_target)
 print('Percent correct: ', 100.0 * correct_predictions / len(predictions))
